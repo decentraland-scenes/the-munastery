1c7bee9367799135bf366747f2c564220131b062
nearnshaw <nearnshaw@decentraland.org>
2019-02-27 17:28:53 -0300

    5.1 migration

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,7 @@ function AddGLTF(path: string, position: Vector3, rotation: Vector3, scale?: Vec
   let entity = new Entity();
 
   let shape = new GLTFShape(path);
-  entity.set(shape);
+  entity.addComponent(shape);
 
   let transform = new Transform();
   transform.position = position;
@@ -13,34 +13,36 @@ function AddGLTF(path: string, position: Vector3, rotation: Vector3, scale?: Vec
   else{
     transform.scale = Vector3.One();
   }
-  entity.set(transform);
+  entity.addComponent(transform);
 
   if (clip){
+    const animator = new Animator();
     let animation = new AnimationClip(clip);
-    shape.addClip(animation);
+    animator.addClip(animation);
+    entity.addComponent(animator);
     animation.play();
   }
 
   engine.addEntity(entity);
 }
 
-AddGLTF("models/Base.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One());
-AddGLTF("models/Tree1.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Tree2.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Tree3.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Tree4.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Tree5.gltf", new Vector3(0, 0, 0), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Water.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One());
-AddGLTF("models/WaterFountain.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One());
-AddGLTF("models/Fire.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "FireBig_Idle");
-AddGLTF("models/B1.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B2.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B3.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B4.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B5.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B6.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B7.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B8.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/B9.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
-AddGLTF("models/Colliders.gltf", new Vector3(0, 0, 0), new Vector3(0, 90, 0), Vector3.One());
-AddGLTF("models/Ground.gltf", new Vector3(0, 0, 0), new Vector3(0, 0, 0), Vector3.One());
+AddGLTF("models/Base.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One());
+AddGLTF("models/Tree1.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Tree2.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Tree3.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Tree4.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Tree5.gltf", new Vector3(20, 0, 20), new Vector3(0, 270, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Water.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One());
+AddGLTF("models/WaterFountain.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One());
+AddGLTF("models/Fire.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "FireBig_Idle");
+AddGLTF("models/B1.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B2.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B3.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B4.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B5.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B6.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B7.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B8.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/B9.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One(), "Armature_Idle");
+AddGLTF("models/Colliders.gltf", new Vector3(20, 0, 20), new Vector3(0, 90, 0), Vector3.One());
+AddGLTF("models/Ground.gltf", new Vector3(20, 0, 20), new Vector3(0, 0, 0), Vector3.One());
